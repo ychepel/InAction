cd21e0628866f76555fde672078a5d452197b42a
Yuriy Chepel <yuriy.chepel@gmail.com>
2016-05-19 13:33:44 +0300

    remove xml properties

--- src/main/java/com/univer/inaction/config/ApplicationConfig.java
+++ src/main/java/com/univer/inaction/config/ApplicationConfig.java
@@ -8,13 +8,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableAspectJAutoProxy
@@ -26,12 +31,37 @@ class ApplicationConfig {
     @Bean
     public PlatformTransactionManager transactionManager()
     {
-        return new JpaTransactionManager(entityManagerFactory());
+        EntityManagerFactory factory = entityManagerFactory().getObject();
+        return new JpaTransactionManager(factory);
     }
 
     @Bean
-    public EntityManagerFactory entityManagerFactory() {
-        return Persistence.createEntityManagerFactory("entityManager");
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean managerFactory = new LocalContainerEntityManagerFactoryBean();
+        managerFactory.setDataSource(dataSource());
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        managerFactory.setJpaVendorAdapter(vendorAdapter);
+        managerFactory.setJpaProperties(additionalProperties());
+
+        return managerFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/inaction");
+        dataSource.setUsername( "postgres" );
+        dataSource.setPassword( "pasw" );
+        return dataSource;
+    }
+
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL82Dialect");
+        return properties;
     }
 
 }
